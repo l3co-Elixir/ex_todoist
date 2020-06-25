0ae85a232b8ca89a79b4604b9230a0dab46fe104
Leandro Costa <lcosta.sp.br@gmail.com>
2020-06-25 10:56:21 -0300

    refactor: update docs

--- lib/todoist/entities/project.ex
+++ lib/todoist/entities/project.ex
@@ -2,6 +2,11 @@ defmodule Todoist.Entities.Project do
 
   @moduledoc """
   Todoist.Entities.Project represent a entity project into Todoist workspace
+
+  ## Example
+
+      iex> %Todoist.Entities.Project{name: "test", color: 36}
+      %Todoist.Entities.Project{ color: 36, favorite: false, name: "test", parent_id: nil }
   """
 
   @derive Jason.Encoder
--- lib/todoist/entities/task.ex
+++ lib/todoist/entities/task.ex
@@ -1,6 +1,11 @@
 defmodule Todoist.Entities.Task do
   @moduledoc """
   Todoist.Entities.Task represent a entity task into Todoist workspace
+
+  ## Example
+
+    iex> %Todoist.Entities.Task{content: "test"}
+    %Todoist.Entities.Task{ assignee: 0, content: "test", due_date: nil, due_datetime: nil, due_lang: nil, due_string: nil, label_ids: nil, order: 0, parent_id: 0, priority: 0, project_id: 0, section_id: 0 }
   """
 
   @derive Jason.Encoder
